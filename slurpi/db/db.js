77c687ae53195ac5136ebdfeac11f743eba57c8f
pstraw <github_main@pstrawberrie.live>
2025-06-09 16:49:33 -0500

    fix NoteLayout css inheritance for list styles, add prod.sqlite for prod

--- slurpi/db/db.js
+++ slurpi/db/db.js
@@ -1,9 +1,11 @@
 import { Sequelize } from 'sequelize';
 import { resolveFromRoot } from '../../path.js';
 
+const dbFile = process.env.NODE_ENV === 'production' ? 'slurpi/db/prod.sqlite' : 'slurpi/db/db.sqlite';
+
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: resolveFromRoot('slurpi/db/db.sqlite'),
+  storage: resolveFromRoot(dbFile),
   logging: false
 });
 

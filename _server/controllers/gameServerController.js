9bef2d4738445b4a3167ab675c1361a583c973a5
pstraw <github_main@pstrawberrie.live>
2025-09-04 09:24:13 -0500

    working example of game servers controller

--- _server/controllers/gameServerController.js
+++ _server/controllers/gameServerController.js
@@ -3,22 +3,33 @@ import { GameDig } from "gamedig";
 const servers = [
   { type: "valheim", host: "pstraw.net" },
   { type: "projectzomboid", host: "pstraw.net" },
+  { type: "farts", host: "asfzzdftest.com" },
 ];
 
 export const getGameServers = async (req, res) => {
   try {
     console.log("GAME SERVER HIT"); //REMOVE
 
-    GameDig.query({
-      type: "valheim",
-      host: "pstraw.net",
-    })
-      .then((state) => {
-        res.json({ success: true, result: state });
+    const queries = [];
+    const output = [];
+
+    servers.forEach((s) => {
+      queries.push(GameDig.query(s));
+    });
+
+    Promise.allSettled(queries)
+      .then((result) => {
+        result.forEach((r) => {
+          if (r.status === "fulfilled") {
+            output.push(r);
+          }
+        });
+
+        res.json({ success: true, result: output });
       })
       .catch((error) => {
-        console.log(`Server is offline: ${error}`);
-        res.json({ error: "no response" });
+        console.log(`Query failed: ${error}`);
+        res.json({ error: "failed" });
       });
   } catch (err) {
     console.error(err);

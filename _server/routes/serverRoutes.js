123a99463bcb979a340dfac81f35365e74ae3184
pstraw <github_main@pstrawberrie.live>
2025-09-08 12:51:52 -0500

    remove gamedig

--- _server/routes/serverRoutes.js
+++ _server/routes/serverRoutes.js
@@ -1,20 +1,14 @@
 import express from "express";
 import {
-  // getSearchStatus,
   postSearch,
   postMediaFilter,
 } from "../controllers/searchController.js";
-import { getGameServers } from "../controllers/gameServerController.js";
 import { postMessage } from "../controllers/messageController.js";
 const router = express.Router();
 
 // Search Routes
-// router.get("/search", getSearchStatus);
 router.post("/search", postSearch);
 router.post("/filter/media", postMediaFilter);
-
-// Game Server Routes
-router.get("/game-servers", getGameServers);
 
 // Message Routes
 router.post("/contact", postMessage);

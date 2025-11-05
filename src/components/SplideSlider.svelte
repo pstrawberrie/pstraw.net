05aeecea48a608117747944bede535f46522c002
pstraw <github_main@pstrawberrie.live>
2025-11-05 17:02:32 -0600

    more slider adjustments, just messin around

--- src/components/SplideSlider.svelte
+++ src/components/SplideSlider.svelte
@@ -8,22 +8,42 @@
     // must be loaded inside effect
     new Splide(sliderEl, {
       type: "loop",
-      gap: "20vw",
-      focus: "center",
+      drag: "free",
+      perPage: 6,
+      focus: 0,
+      breakpoints: {
+        500: {
+          perPage: 2,
+        },
+        680: {
+          perPage: 3,
+        },
+        850: {
+          perPage: 4,
+        },
+        1100: {
+          perPage: 5,
+        },
+      },
     }).mount();
   });
 </script>
 
-<section
-  class="splide"
-  aria-label="Splide Basic HTML Example"
-  bind:this={sliderEl}
->
+<section class="splide" aria-labelledby="carousel-heading" bind:this={sliderEl}>
+  <h2 id="carousel-heading">Splide Basic HTML Example</h2>
+
   <div class="splide__track">
     <ul class="splide__list">
       <li class="splide__slide">Slide 01</li>
       <li class="splide__slide">Slide 02</li>
       <li class="splide__slide">Slide 03</li>
+      <li class="splide__slide">Slide 04</li>
+      <li class="splide__slide">Slide 05</li>
+      <li class="splide__slide">Slide 06</li>
+      <li class="splide__slide">Slide 07</li>
+      <li class="splide__slide">Slide 08</li>
+      <li class="splide__slide">Slide 09</li>
+      <li class="splide__slide">Slide 10</li>
     </ul>
   </div>
 </section>

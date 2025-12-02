43c645f490ba9b10f3cbcfed9443b73cb4511321
pstraw <github_main@pstrawberrie.live>
2025-12-02 13:07:26 -0600

    update astro, move some things around to start testing a new theme

---
+++ src/components/ActivityHero.svelte
@@ -0,0 +1,23 @@
+<script>
+  let { test } = $props();
+</script>
+
+<div id="activity-hero">
+  <div class="inner"></div>
+</div>
+
+<style lang="scss">
+  #activity-hero {
+    position: relative;
+    display: flex;
+    align-items: center;
+    min-height: 100vh;
+    background: var(--full-hero-gradient);
+    padding: var(--space-box-v) 0;
+    overflow: hidden;
+  }
+
+  .inner {
+    position: relative;
+  }
+</style>
